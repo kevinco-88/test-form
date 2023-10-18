6430860a5f63d6c423957229035bd5af922e40de
kevinco-88 <85828868+kevinco-88@users.noreply.github.com>
2023-10-18 18:16:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,367 @@
+const data = {
+  data1: {
+    question: " I ...... from Albania.",
+    a: "is",
+    b: "are",
+    c: "am",
+    d: "be",
+    answer: "c",
+  },
+  data2: {
+    question: "My father is ...... artist.",
+    a: "the",
+    b: "an",
+    c: "a",
+    d: "for",
+    answer: "b",
+  },
+  data3: {
+    question: "This is my friend. ...... name is Ana.",
+    a: "His",
+    b: "Our",
+    c: "Your",
+    d: "Her",
+    answer: "d",
+  },
+  data4: {
+    question: "Alex is ......",
+    a: "my brother’s friend",
+    b: "friend my brother",
+    c: "friend from my brother",
+    d: "my brother friends",
+    answer: "a",
+  },
+  data5: {
+    question: "...... 25 chairs in the classroom. ",
+    a: "This is",
+    b: "There is",
+    c: "There are",
+    d: "They are",
+    answer: "c",
+  },
+  data6: {
+    question: "My sister ...... horror movies.",
+    a: "don’t like",
+    b: "likes not",
+    c: "doesn’t like",
+    d: "isn’t like",
+    answer: "c",
+  },
+  data7: {
+    question: " Sorry, I can’t talk. I ...... right now.",
+    a: "Is driving ",
+    b: "‘m driving",
+    c: "driving",
+    d: "drive",
+    answer: "b",
+  },
+  data8: {
+    question: "He ...... at work yesterday. ",
+    a: "didn’t be",
+    b: "weren’t",
+    c: "wasn’t",
+    d: "doesn’t go",
+    answer: "c",
+  },
+  data9: {
+    question: "I ...... the film last night. ",
+    a: "liked",
+    b: "likes",
+    c: "don’t like",
+    d: "like",
+    answer: "a",
+  },
+  data10: {
+    question: "...... a piece of cake? No, thank you",
+    a: "Want you",
+    b: "Do you like",
+    c: "Would you like",
+    d: "Want like",
+    answer: "c",
+  },
+  data11: {
+    question: "The kitchen is ...... than the bedroom.",
+    a: "more small",
+    b: "smaller",
+    c: "smallest",
+    d: "more smaller",
+    answer: "b",
+  },
+  data12: {
+    question: "Ann is a vegetarian, she ...... meat.",
+    a: "sometimes eats",
+    b: "never eats",
+    c: "often eats",
+    d: "usually eats",
+    answer: "b",
+  },
+  data13: {
+    question: "There aren’t ...... eggs in the fridge",
+    a: "any",
+    b: "some",
+    c: "no",
+    d: "a",
+    answer: "a",
+  },
+  data14: {
+    question: "The car park is ...... to the hotel. ",
+    a: "between",
+    b: "behind",
+    c: "next",
+    d: "in front",
+    answer: "c",
+  },
+  data15: {
+    question: "They ...... in the park when it started to rain.",
+    a: "played",
+    b: "are playing",
+    c: "were playing",
+    d: "were play",
+    answer: "c",
+  },
+  data16: {
+    question: "...... seen a falling star before?",
+    a: "Did you ever",
+    b: "Are you ever",
+    c: "Have you ever",
+    d: "Do you ever",
+    answer: "c",
+  },
+  data17: {
+    question: "We’ve been married ...... many years.",
+    a: "since",
+    b: "from",
+    c: "during",
+    d: "for",
+    answer: "d",
+  },
+  data18: {
+    question: "You ...... pay for the tickets. They’re free.",
+    a: "have to",
+    b: "don’t have",
+    c: "don’t need to",
+    d: "doesn’t have to",
+    answer: "c",
+  },
+  data19: {
+    question: "Ben was ill last week and he ...... go out.",
+    a: "needn’t",
+    b: "can’t",
+    c: "mustn’t",
+    d: "couldn’t",
+    answer: "d",
+  },
+  data20: {
+    question: "These are the pictures ...... I took on our vacation.",
+    a: "which",
+    b: "who",
+    c: "what",
+    d: "where",
+    answer: "a",
+  },
+  data21: {
+    question: "We’ll stay at home if it ...... tomorrow.",
+    a: "raining",
+    b: "rains",
+    c: "will rain",
+    d: "rain",
+    answer: "b",
+  },
+  data22: {
+    question: "He doesn’t smoke now, but he ...... a lot when he was young.",
+    a: "has smoked",
+    b: "smokes",
+    c: "used to smoke",
+    d: "was smoked",
+    answer: "c",
+  },
+
+  data23: {
+    question: "This city ...... by lots of tourists during the winter.",
+    a: "visits",
+    b: "visited",
+    c: "is visiting",
+    d: "is visited",
+    answer: "d",
+  },
+  data24: {
+    question: "I wasn’t interested in the performance very much. ...... ",
+    a: "I didn’t, too.",
+    b: "Neither was I.",
+    c: "Nor I did.",
+    d: "So I wasn’t.",
+    answer: "b",
+  },
+  data25: {
+    question:
+      "She ...... for her cat for two days when she finally found it in the garage.",
+    a: "looked",
+    b: "had been looked",
+    c: "had been looking",
+    d: "were looking",
+    answer: "c",
+  },
+  data26: {
+    question: "Before she got married, she…….. every summer travelling. ",
+    a: "Spent",
+    b: "Was spending",
+    c: "Spend",
+    d: "Wasn’t spanding",
+    answer: "a",
+  },
+  data27: {
+    question: "If I hadn’t replied to your email, I ...... here with you now.",
+    a: "can’t be",
+    b: "wouldn’t be",
+    c: "won’t be",
+    d: "haven’t been",
+    answer: "b",
+  },
+  data28: {
+    question: "I think it’s very easy to ...... debt these days.",
+    a: "go into",
+    b: "become",
+    c: "go down to",
+    d: "get into",
+    answer: "d",
+  },
+  data29: {
+    question: `Read and select the best option. </br></br>
+
+            <span><i>Despite the widespread benefits of immunization, there is a section of society that holds onto the belief that vaccinations could lead to unwanted side effects.</br> The misconceptions and fear often hinder efforts to eradicate certain preventable diseases.</br> What is the main obstacle in eradicating preventable diseases, as per the text?<span><i>`,
+    a: "The benefits of immunization.",
+    b: "Misconceptions and fear regarding vaccinations",
+    c: "Unwanted side effects of vaccinations.",
+    d: "The belief that vaccinations are unnecessary.",
+    answer: "b",
+  },
+  data30: {
+    question: `Read and select the best option. </br></br>
+            <span>
+           <i>While numerous studies underscore the relationship between education and societal development, we must be cautious not to mistake correlation for causation.</br> The evolution of society is the result of a complex interplay of numerous factors, of which education is undoubtedly an integral part, but not the only driving force.
+            </br>According to the text, what is the role of education in societal development?</i></span>`,
+    a: "Education is the sole driving force of societal development.",
+    b: "Education has no role in societal development.",
+    c: "Education is an integral part, but not the only factor driving societal development.",
+    d: "The role of education in societal development is entirely misunderstood.",
+    answer: "c",
+  },
+};
+
+const question = document.querySelector(".question");
+const answer1 = document.querySelector("#answerA");
+const answer2 = document.querySelector("#answerB");
+const answer3 = document.querySelector("#answerC");
+const answer4 = document.querySelector("#answerD");
+const allanswers = document.querySelectorAll(".answers");
+const pyetja = document.querySelector(".pyetja");
+const contLanguage = document.querySelector(".language");
+const albFlag = document.querySelector(".shqip");
+const enFlag = document.querySelector(".english");
+
+const maxDataNumber = Object.keys(data).length;
+
+let points = 0;
+
+let dataNumber = 1;
+
+let level = "";
+
+function showContent() {
+  question.innerHTML = data[`data${dataNumber}`].question;
+  answer1.innerHTML = data[`data${dataNumber}`].a;
+  answer2.innerHTML = data[`data${dataNumber}`].b;
+  answer3.innerHTML = data[`data${dataNumber}`].c;
+  answer4.innerHTML = data[`data${dataNumber}`].d;
+  pyetja.innerHTML = `${dataNumber}/30`;
+}
+
+showContent();
+
+const primeEn = "Maybe you should start learning english";
+const primeAl = "Mendoj se duhet të nisni të mësoni anglisht";
+const enCont = "Based on the points obtained, your level is ";
+const alCont = "Niveli juaj ne baze te pikeve eshte ";
+
+let activePrime = "";
+
+let activeCont = "";
+
+let activePoint = "";
+
+let activeLanguage = "shqip";
+
+function activeFlag() {
+  if (activeLanguage == "shqip") {
+    enFlag.style.opacity = "0.3";
+    albFlag.style.opacity = "1";
+    activePrime = primeAl;
+    activeCont = alCont;
+  } else if (activeLanguage == "english") {
+    albFlag.style.opacity = "0.3";
+    enFlag.style.opacity = "1";
+    activePrime = primeEn;
+    activeCont = enCont;
+  }
+}
+
+activeFlag();
+
+contLanguage.addEventListener("click", function (e) {
+  if (
+    e.target.getAttribute("class") == "shqip" ||
+    e.target.getAttribute("class") == "english"
+  ) {
+    activeLanguage = e.target.getAttribute("class");
+    activeFlag();
+    languageShown();
+  }
+});
+
+function languageShown() {
+  if (dataNumber !== 0) return null;
+  if (activeLanguage == "shqip") {
+    question.innerHTML = `Ju keni marrë ${points} pikë. </br>${activeCont} ${level}`;
+  } else if (activeLanguage == "english") {
+    question.innerHTML = `You have ${points} points. </br>${activeCont} ${level}`;
+  }
+}
+
+function clearContent() {
+  dataNumber = 0;
+  languageShown();
+  allanswers.forEach((x) => (x.style.display = "none"));
+  pyetja.style.display = "none";
+}
+
+function showResult() {
+  if (points <= 4) {
+    level = `${activePrime}`;
+  } else if (points <= 10) {
+    level = `a1`;
+  } else if (points <= 17) {
+    level = `a2`;
+  } else if (points <= 24) {
+    level = `b1`;
+  } else if (points <= 29) {
+    level = `b2`;
+  } else if (points == 30) {
+    level = `c1`;
+  }
+}
+
+allanswers.forEach((x) =>
+  x.addEventListener("click", function (e) {
+    if (data[`data${dataNumber}`].answer === e.target.getAttribute("answer")) {
+      points++;
+    }
+    if (dataNumber >= maxDataNumber) {
+      showResult();
+      clearContent();
+      return null;
+    }
+    dataNumber++;
+
+    showContent();
+  })
+);
